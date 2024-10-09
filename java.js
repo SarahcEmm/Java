6539de68dbdef581c46d45a1ac0df0d54074be44
Sarah Moorby <lhijd8vq@students.codeinstitute.net>
2024-10-09 23:11:07 +0000

    added background colours

--- java.js
+++ java.js
@@ -1,16 +1,5 @@
-const body = document.getElementsByTagName("body")[0]
-function setColor(name) {
-    body.style.backgroundColor = name;
-}
-function randomColor () {
-    const red = Math.round(Math.random() * 255)
-    const green = Math.round(Math.random() * 255)
-    const blue = Math.round(Math.random() * 255)
 
-    const color = `rgb(${red}, ${green}, ${blue})`
-    body.style.backgroundColor = color;
-}
-randomColor()
+
 
 const input = document.getElementById ("input")
 
